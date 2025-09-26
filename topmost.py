a435dfc1c5eafd771f6c00613dc0ed9c5d6eab9f
a23arvme <a23arvme@student.his.se>
2025-09-26 14:09:10 +0200

    tiny fix

--- topmost.py
+++ topmost.py
@@ -3,14 +3,16 @@ import wordfreq
 import urllib.request 
 
 def main():
-	stopwords = open(sys.argv[1]).readlines()
+	stopwords = open(sys.argv[1])
+	stopword_lines = stopwords.readlines()
+
 	response = urllib.request.urlopen(sys.argv[2])
 	lines = response.read().decode("utf8").splitlines()
 
 	count = int(sys.argv[3])
 
 	result = wordfreq.tokenize(lines)
-	words = wordfreq.countWords(result, stopwords)
+	words = wordfreq.countWords(result, stopword_lines)
 	wordfreq.printTopMost(words, count)
 
 	stopwords.close()
